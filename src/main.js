148ac54494e787fe18dc591adc792d9a1a3d4c92
whdgus906 <whdgus906@gmail.com>
2016-11-30 17:26:43 +0900

    자동 로그인 처리

--- src/main.js
+++ src/main.js
@@ -52,7 +52,7 @@ app.on('ready', () =>{
         )
     })
     
-    //쿠키값 수정. 크롬에서 구동할 때, 추가 플레이어 팝업이 안나오게 수정. 근데 electron에서 켜지는 지니가 사파리기반인지 크롬기반인지 확인 후 수정할 예정..
+    //쿠키값 수정. 크롬에서 구동할 때, 추가 플레이어 팝업이 안나오게 수정. electron에서 켜지는 지니가 사파리기반인지 크롬기반인지 확인 후 수정할 예정
     /*
     session.defaultSession.cookies.set({url:'http://www.genie.co.kr/', 
                                             name:'musicPlayer',
@@ -62,21 +62,32 @@ app.on('ready', () =>{
     
     //결제하지 않은 계정으로 로그인할 때 웹플레이어에서 프로모션페이지로 리다이렉트되는 문제 처리
     mainWindow.webContents.on('did-get-response-details', (event,status,newURL,originalURL,httpResponseCode,requestMethod,referrer,headers,resourceType)=>{
-        if(newURL.indexOf("/buy/promotionConfirm?pxnm") != -1){
+        if(newURL.indexOf("/buy/promotionConfirm?pxnm") != -1 || newURL == "http://www.genie.co.kr/"){
             mainWindow.loadURL(`http://www.genie.co.kr/player/fPlayer`)
         }
+        
+        
     })
     
-    
+    // 로그인 후 메인홈페이지로 이동하는 문제 처리
     mainWindow.webContents.on('new-window',(event, url,disposition,additionalFeatures, options) =>{
-       
         if(disposition == "genie_main" || url == 'http://www.genie.co.kr/player/fPlayer'){
             options.show = false
             setTimeout(function(){mainWindow.reload()},500)
         }
     })
     
+    mainWindow.webContents.on("did-get-redirect-request", (newURL, event) =>{
+     //console.log(url.Url)
+        if(url == "http://www.genie.co.kr"){
+            console.log("메인 이동 확인")
+            mainWindow.loadURL("http://www.genie.co.kr/player/fPlayer")
+        }
+    })
+     
 })
+
+ 
 
 
 app.on('window-all-closed', function () {
